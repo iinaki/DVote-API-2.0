1793a46be078a835d6b9ab48547dd64a92fa00d5
iinaki <llorensinaki22@gmail.com>
2024-05-13 13:01:20 -0300

    db running locally

--- src/db.py
+++ src/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, MetaData
 
 
-DATABASE_URL = "postgresql://dvote_user:aD7nniFvRBRiCc4rkSTBWcypCQh18yka@dpg-cp0nkta1hbls73edkjc0-a.ohio-postgres.render.com/dvote"
+DATABASE_URL = "postgresql+psycopg2://dvote_user:aD7nniFvRBRiCc4rkSTBWcypCQh18yka@dpg-cp0nkta1hbls73edkjc0-a.ohio-postgres.render.com/dvote"
 
 # class Base(DeclarativeBase):
 #     pass
