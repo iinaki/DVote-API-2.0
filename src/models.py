f63d6be27925a66a70d3652333fd8519a360ad03
iinaki <llorensinaki22@gmail.com>
2024-05-18 01:33:50 -0300

    updates in API

--- src/models.py
+++ src/models.py
@@ -2,6 +2,12 @@ from sqlalchemy import Table, Column
 from sqlalchemy.sql.sqltypes import Integer, String, Boolean 
 from src.db import meta, engine
 
-users = Table('user', meta, Column('id', Integer, primary_key=True), Column('sha_dni', Integer), Column('voto', Boolean), Column('lugar_residencia', String(255)))
+users = Table(
+    'user', meta,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('sha_dni', BigInteger),
+    Column('voto', Boolean),
+    Column('lugar_residencia', String(255))
+)
 
 meta.create_all(bind=engine)
